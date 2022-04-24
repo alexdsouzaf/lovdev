c033075d4bd720810d2655d9a8e481648b90b02f
Alexsander Faria <alexsander.itj@hotmail.com>
2022-04-24 13:37:32 -0300

    Criado UserModel em uma pasta para modelos.
    reorganizando o layout conforme previa do readme.

--- src/xamarin/LovDev/LovDev/LovDev/MainPage.xaml.cs
+++ src/xamarin/LovDev/LovDev/LovDev/MainPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using System.Collections.ObjectModel;
+using LovDev.Models;
 
 namespace LovDev
 {
@@ -14,7 +15,7 @@ namespace LovDev
     {
         
  
-        public ObservableCollection<UserProfile> _Profiles = new ObservableCollection<UserProfile>();
+        public ObservableCollection<UserModel> _Profiles = new ObservableCollection<UserModel>();
 
         public MainPage()
         {
@@ -26,25 +27,24 @@ namespace LovDev
 
         public void CardBinding()
         {
-            _Profiles.Add(new UserProfile() { Name = "João", Age = "25", Photo = "AvatarPlaceholder.png" });
-            _Profiles.Add(new UserProfile() { Name = "Maria", Age = "20", Photo = "pic_2.jpg" });
-            _Profiles.Add(new UserProfile() { Name = "José", Age = "30", Photo = "pic_1.png" });
-            _Profiles.Add(new UserProfile() { Name = "Fernando", Age = "18", Photo = "AvatarPlaceholder.png" });
-            _Profiles.Add(new UserProfile() { Name = "Sofia", Age = "21", Photo = "pic_2.jpg" });
-            _Profiles.Add(new UserProfile() { Name = "Yuno", Age = "20", Photo = "AvatarPlaceholder.png" });
-            _Profiles.Add(new UserProfile() { Name = "Alex", Age = "25", Photo = "pic_1.jpg" });
-            _Profiles.Add(new UserProfile() { Name = "Thales", Age = "23", Photo = "pic_1.jpg" });
-            _Profiles.Add(new UserProfile() { Name = "Abner", Age = "32", Photo = "AvatarPlaceholder.png" });
-            _Profiles.Add(new UserProfile() { Name = "Marcos", Age = "27", Photo = "pic_1.jpg" });
-            _Profiles.Add(new UserProfile() { Name = "Paulo", Age = "40", Photo = "pic_1.jpg" });
-            _Profiles.Add(new UserProfile() { Name = "Bruna", Age = "31", Photo = "pic_2.jpg" });
-            _Profiles.Add(new UserProfile() { Name = "Janaína", Age = "24", Photo = "AvatarPlaceholder.png" });
-            _Profiles.Add(new UserProfile() { Name = "Luana", Age = "22", Photo = "pic_2.jpg" });
+            _Profiles.Add(new UserModel() { Name = "João", Age = "25",About = "...", Photo = "AvatarPlaceholder.png" });
+            _Profiles.Add(new UserModel() { Name = "Maria", Age = "20", About = "...", Photo = "pic_2.jpg" });
+            _Profiles.Add(new UserModel() { Name = "José", Age = "30", About = "...", Photo = "pic_1.png" });
+            _Profiles.Add(new UserModel() { Name = "Fernando", Age = "18", About = "...", Photo = "AvatarPlaceholder.png" });
+            _Profiles.Add(new UserModel() { Name = "Sofia", Age = "21", About = "...", Photo = "pic_2.jpg" });
+            _Profiles.Add(new UserModel() { Name = "Yuno", Age = "20",  About = "...",Photo = "AvatarPlaceholder.png" });
+            _Profiles.Add(new UserModel() { Name = "Alex", Age = "25",  About = "...",Photo = "pic_1.jpg" });
+            _Profiles.Add(new UserModel() { Name = "Thales", Age = "23",About = "...",Photo = "pic_1.jpg" });
+            _Profiles.Add(new UserModel() { Name = "Abner", Age = "32", About = "...",Photo = "AvatarPlaceholder.png" });
+            _Profiles.Add(new UserModel() { Name = "Marcos", Age = "27",About = "...",Photo = "pic_1.jpg" });
+            _Profiles.Add(new UserModel() { Name = "Paulo", Age = "40", About = "...",Photo = "pic_1.jpg" });
+            _Profiles.Add(new UserModel() { Name = "Bruna", Age = "31", About = "...",Photo = "pic_2.jpg" });
+            _Profiles.Add(new UserModel() { Name = "Janaína", Age = "24", About = "...", Photo = "AvatarPlaceholder.png" });
+            _Profiles.Add(new UserModel() { Name = "Luana", Age = "22", About = "...", Photo = "pic_2.jpg" });
 
         }
     
-
-        public ObservableCollection<UserProfile> Profile
+        public ObservableCollection<UserModel> Profile
         {
             get => _Profiles;
             set
@@ -52,13 +52,5 @@ namespace LovDev
                 _Profiles = value;
             }
         }
-
-        public class UserProfile
-        {
-            public string Name { get; set; }
-            public string Age { get; set; }
-            public string Photo { get; set; }
-        }
-
     }
 }
---
+++ src/xamarin/LovDev/LovDev/LovDev/Models/UserModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LovDev.Models
+{
+    public class UserModel
+    {
+        
+        /// <summary>
+        /// Nome
+        /// </summary>
+        public string Name { get; set; }
+        
+        /// <summary>
+        /// Idade
+        /// </summary>
+        public string Age { get; set; }
+        
+        /// <summary>
+        /// Descrição curto
+        /// </summary>
+        public string About { get; set; }
+        
+        /// <summary>
+        /// Localização
+        /// Para apresentar a distancia
+        /// </summary>
+        public string Location { get; set; }
+        
+        /// <summary>
+        /// linguagens e tecnologias
+        /// </summary>
+        public string[] stacks { get; set; } 
+        
+        /// <summary>
+        /// Foto
+        /// </summary>
+        public string Photo { get; set; } //provavelmente esse campo da imagem vai deixar de ser string , talvez um stream?
+    }
+}
